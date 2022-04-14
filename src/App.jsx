4c05fd97b26c108df4ef248e63cd00aeabce63c1
Julio Henrique <juliohjesus@gmail.com>
2022-04-14 17:52:52 -0300

    doing menu

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import './App.css';
+
+function App() {
+  return (
+    <main>
+      <section className='menu section'>
+        <div className='title'>
+          <h2>Our Menu</h2>
+          <div className='underline'></div>
+        </div>
+      </section>
+      aqui vem os componentes, o categories filtra os items
+    </main>
+  )
+}
+
+export default App;
